6383a503e8767cd132e1f36eac2e554fb8ba975f
rachelglasser <38412318+rachelglasser@users.noreply.github.com>
2020-12-28 12:39:24 -0600

    Update transform-active.js

--- transform-active.js
+++ transform-active.js
@@ -1,6 +1,9 @@
 // Keeps track of the original positions of all the items
+// And onclick functions
 var orig_coordinates = [];
+var orig_onclicks = {};
 $(".item").each(function(index, item) {
+	// Original coordinates
 	var randomWidth = Math.floor(Math.random() * $(window).width() * .9);
 	var randomHeight = Math.floor(Math.random() * $(window).height() * .9)
 	orig_coordinates.push({
@@ -9,8 +12,10 @@ $(".item").each(function(index, item) {
 	});
 });
 
+var isMobile = $(window).width() > 600;
 var front_z_index = 0;
 
+// To use in onclick functions to make sure it doesn't open on drag stop
 function openLink(link, itemId) {
 	if ($(itemId).hasClass('noclick')) {
 		return
@@ -20,9 +25,10 @@ function openLink(link, itemId) {
 
 // Go through all of the items
 $(".item").each(function(index, item) {
-	// Changes the positions to hard-code the starting positions (so that everything isn't messed up with drag/drop)
-	item.style = 'left:'+orig_coordinates[index].left+'px; top: '+orig_coordinates[index].top+'px; position: absolute;';
-	var itemButton = $("#"+item.id+" button")
+	if (isMobile) {
+		// Changes the positions to hard-code the starting positions (so that everything isn't messed up with drag/drop)
+		item.style = 'left:'+orig_coordinates[index].left+'px; top: '+orig_coordinates[index].top+'px; position: absolute;';
+	}
 
 	// Add lightbox
 	$('#'+item.id).find('img').wrap(function() {
@@ -30,88 +36,38 @@ $(".item").each(function(index, item) {
 	});
 
 	// When you click the menu button
-	itemButton.click(function(event) {
+	$(this).children('button').click(function(event) {
 		front_z_index++;
 		$('#'+item.id).css('z-index',front_z_index);
-		if (item.classList.contains('noclick')) {
-			return;
+		if ($('#'+item.id).hasClass('noclick')) {
+			return
 		}
-		var imageQueryString = "#"+item.id+" img";
 		item.classList.toggle('open');
-
-		// Go through all of the image tags for that item
-		$(imageQueryString).each(function(index, image) {
-			// Adds class "open" to this image when the item is open
-			// Removes class "open" when you close the item
-			image.classList.toggle("open");
-
-			// interval is in milliseconds, so this
-			// runs the function every 15 ms
-			var interval = 15;
-			var maxWidth = 80;
-			var minItemWidth = 15;
-			var maxItemWidth = 35;
-			// Width is set to the percent of the width of the grey box
-			var imgWidth;
-			var itemWidth;
-			if (image.classList.contains("open")) {
-				imgWidth = 0;
-				itemWidth = minItemWidth;
-			} else {
-				imgWidth = maxWidth;
-				itemWidth = maxItemWidth;
-			}
-			// Amount you need to change overall divided by the number of 
-			// intervals
-			var itemWidthRate = (maxItemWidth - minItemWidth) / (maxWidth)
-			var id = setInterval(frame, interval);
-			function frame() {
-				// increase width if the item is opening
-				if (image.classList.contains("open")) {
-					if (imgWidth >= maxWidth) {
-						clearInterval(id);
-					} else {
-						imgWidth += 1;
-						itemWidth += itemWidthRate;
-						image.style = "width:"+imgWidth+"%; padding-bottom: 10%";
-						$('#'+item.id).css('width',itemWidth+'%')
-					}
-				} else {
-					if (imgWidth <= 0) {
-						image.style = "width:"+imgWidth+"%; padding-bottom: 0%;"
-						clearInterval(id);
-					} else {
-						imgWidth -= 1;
-						itemWidth -= itemWidthRate;
-						image.style = "width:"+imgWidth+"%; padding-bottom: 10%;";
-						$('#'+item.id).css('width',itemWidth+'%')
-					}
-				}
-			}
-		});
 	})
 });
 
-// Make the items draggable
-$('.item').draggable({ 
-	cancel: true,
-	start: function(event, ui) {
-		front_z_index++;
-		$(this).css('z-index', front_z_index);
-		if ($(this)[0].classList.contains('open')
-			|| $(this)[0].classList.contains('linkitem')) {
-			$(this).addClass('noclick');
+if (isMobile) {
+	// Make the items draggable
+	$('.item').draggable({ 
+		cancel: true,
+		start: function(event, ui) {
+			front_z_index++;
+			$(this).css('z-index', front_z_index);
+			if ($(this).hasClass('linkitem')) {
+				$(this).children('button').addClass('noclick');
+			}
+		},
+		stop: function(event, ui) {
+			var item = $(this);
+			$(ui.helper).css('width','-webkit-fill-available')
+			item.css('height','auto');
+			if (item.hasClass('open') || item.hasClass('linkitem')) {
+				item.addClass('noclick');
+			}
+			setTimeout(function() {
+				item.removeClass('noclick');
+			},
+			500)
 		}
-	},
-	stop: function(event, ui) {
-		$(this).css('height','auto');
-		if ($(this)[0].classList.contains('open')
-			|| $(this)[0].classList.contains('linkitem')) {
-			$(this).addClass('noclick');
-		}
-		var currentItem = $(this);
-		setTimeout(function() {
-			currentItem.removeClass('noclick')
-		}, 30);
-	}
-});
+	});
+}
